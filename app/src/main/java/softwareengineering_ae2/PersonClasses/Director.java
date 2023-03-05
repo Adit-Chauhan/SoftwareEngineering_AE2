1e82dfaaaae823f5993ae7ac1b743285ef50a6ea
Justin-CC <justinchansw@gmail.com>
2023-03-05 02:17:46 +0000

    persons

---
+++ app/src/main/java/softwareengineering_ae2/PersonClasses/Director.java
@@ -0,0 +1,12 @@
+package CourseWork;
+
+public class Director extends Staff {
+
+	public Director(String n, int a, int id, String title) {
+		super(n, a, id, title);
+	}
+
+	public void addRequirments(StudentCourseRequirements s) {
+		//to be added
+	}
+}
