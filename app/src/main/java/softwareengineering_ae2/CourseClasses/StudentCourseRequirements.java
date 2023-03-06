5c91ce365463119d5adb7c520cf81c40e636931a
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-03-06 18:44:11 +0000

    Tacked on Database serializer

--- app/src/main/java/softwareengineering_ae2/CourseClasses/StudentCourseRequirements.java
+++ app/src/main/java/softwareengineering_ae2/CourseClasses/StudentCourseRequirements.java
@@ -1,12 +1,16 @@
 package softwareengineering_ae2.CourseClasses;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 import java.util.TreeSet;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class StudentCourseRequirements extends Course {
 	private static int totalStudentCourseIDs = 0;
 	private String teacher; // TODO: Answer Q: Make a reference to a Teacher object instead? i.e how much information is required? // private PartTimeTeacher teacher;
+							// TODO: ADIT:: I think we can it may prevent useless data duplication (that said i am not aware of the rules of when java will copy data)
 	private TimetableDate studentCourseDateTime;
-	private TreeSet<String> requiredSkills;
+	private TreeSet<String> requiredSkills; // Q: ADIT, Just curious as to why did you use TreeSet is the order of skills important?
 
 	//TODO: Answer Q, maybe add more constructors
 	// Q: >1 constructor required (with accompanying getters/setters?) if we don't have all these details??) Or do we not want anyone to modify the specific course once it is created?
@@ -38,7 +42,4 @@ public class StudentCourseRequirements extends Course {
 		return requiredSkills;
 	}
 	
-
-
-
 }
