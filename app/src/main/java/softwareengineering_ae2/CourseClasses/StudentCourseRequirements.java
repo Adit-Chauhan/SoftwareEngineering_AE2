48eed8d6683871552aaf1757ef943c9c82bef6d8
LynetteSki <126945534+LynetteSki@users.noreply.github.com>
2023-03-08 08:28:37 +0000

    TreeSet -> HashSet
    Change made for more efficient search/retrieval

--- app/src/main/java/softwareengineering_ae2/CourseClasses/StudentCourseRequirements.java
+++ app/src/main/java/softwareengineering_ae2/CourseClasses/StudentCourseRequirements.java
@@ -2,7 +2,7 @@ package softwareengineering_ae2.CourseClasses;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
-import java.util.TreeSet;
+import java.util.HashSet;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class StudentCourseRequirements extends Course {
@@ -10,11 +10,11 @@ public class StudentCourseRequirements extends Course {
 	private PartTimeTeacher teacher; //@Justin: This was my (Lynette) question lol.  // DONE: Answer Q: Make a reference to a Teacher object instead? i.e how much information is required? // 
 							// DONE: ADIT:: I think we can it may prevent useless data duplication (that said i am not aware of the rules of when java will copy data)
 	private TimetableDate studentCourseDateTime;
-	private TreeSet<String> requiredSkills; // A: The order of the skills isn't important - what is important is that these skills shouldn't be duplicated. ALthough maybe a hashSet mayindeed be better for retrieval? Q: ADIT, Just curious as to why did you use TreeSet is the order of skills important?
+	private HashSet<String> requiredSkills; // A: The order of the skills isn't important - what is important is that these skills shouldn't be duplicated. ALthough maybe a hashSet mayindeed be better for retrieval? Q: ADIT, Just curious as to why did you use TreeSet is the order of skills important?
 
 	//TODO: Answer Q, maybe add more constructors
 	// Q: >1 constructor required (with accompanying getters/setters?) if we don't have all these details??) Or do we not want anyone to modify the specific course once it is created?
-	public StudentCourseRequirements(String inputCourseName, String inputCourseDesc, TimetableDate inputTime, TreeSet<String> inputRequiredSkills){
+	public StudentCourseRequirements(String inputCourseName, String inputCourseDesc, TimetableDate inputTime, HashSet<String> inputRequiredSkills){
 		super(inputCourseName, inputCourseDesc);
 		
 		this.courseID = totalStudentCourseIDs++;
@@ -38,7 +38,7 @@ public class StudentCourseRequirements extends Course {
 	} 
 
 	// Returns total list of requirements
-	public TreeSet<String> getSkillsRequired(){
+	public HashSet<String> getSkillsRequired(){
 		return requiredSkills;
 	}
 	
