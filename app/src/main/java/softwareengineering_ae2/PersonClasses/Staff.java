2f3c7da006a1dcffc2a02f760d0f0066653e2a32
Justin-CC <justinchansw@gmail.com>
2023-03-13 21:20:50 +0000

    updated

---
+++ app/src/main/java/softwareengineering_ae2/PersonClasses/Staff.java
@@ -0,0 +1,14 @@
+
+abstract class Staff extends Person{
+	private int id;
+	private String title;
+	
+	public Staff (Stirng name, int age, int id, String title) {
+		super(name, age);
+		this.id = id;
+		this.title = title;
+	}
+	
+	
+	
+}
