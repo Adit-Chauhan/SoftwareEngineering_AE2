0caa94c8da86b63d57bd4db257659dc37e0d61a8
Ashwin Nanda <2772570n@student.gla.ac.uk>
2023-03-14 11:53:31 +0000

    timetabledate class created

---
+++ app/src/main/java/softwareengineering_ae2/TimetableDate/TimetableDate.java
@@ -0,0 +1,25 @@
+package main.java.softwareengineering_ae2.TimetableDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class TimetableDate{
+
+    ZonedDateTime courseScheduledTime;
+    DateTimeFormatter courseScheduledTimeFormatter;
+    String formattedCourseScheduledTime;
+
+
+
+    public String getFormattedCourseScheduled(){
+
+        formattedCourseScheduledTime =  DateTimeFormatter.ISO_LOCAL_DATE_TIME(courseScheduledTime);
+        return formattedCourseScheduledTime;
+
+    }
+
+
+
+
+}
+
