8bdf399a255a11d07000a912aca41f68c98243b8
Ashwin Nanda <2772570n@student.gla.ac.uk>
2023-03-14 12:04:04 +0000

    skeleton admin

---
+++ app/src/main/java/softwareengineering_ae2/PersonClasses/Administrator.java
@@ -0,0 +1,42 @@
+package main.java.softwareengineering_ae2.PersonClasses;
+import softwareengineering_ae2;
+public class Administrator {
+    int AdminID;
+    ArrayList<PartTimeTeacher> allPartTimeTeachingStaff;
+    ArrayList<StudentCourse> allStudentCourses;
+    ArrayList<TeacherTrainingCourses> allTeacherTrainingCourses;
+   
+    public void readStudentCourseInfo(){
+
+    }
+    
+
+
+
+
+    
+    public void readTeacherTrainingInfo(){
+
+    }
+
+
+
+    
+    public void readPartTimeTeacherInfo(){
+
+    }
+
+
+    
+    public void matchTeachersToStudentCourse(Course course, ArrayList<Teacher> teacher){
+
+    }
+
+
+
+
+    public void matchTeachersToTeacherTraining(Teacher teacher, ArrayList<TeacherTrainingCourse> ttc){
+
+    }
+   
+}
